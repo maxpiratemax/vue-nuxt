3c07ae6c73c430b177bef7046553a8939cc5d9e0
Max <max_pirate@mail.ru>
2022-08-08 01:14:26 +0300

    Create event page/404page

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,13 @@ module.exports = {
   extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  // rules: { 'vue/multi-word-component-names': 'off',},
+  overrides: [
+    {
+        files: ['pages/**/*.vue', 'layouts/*.vue'],
+        rules: {
+            'vue/multi-word-component-names': 'off',
+        },
+    },
+],
 }
