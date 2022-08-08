f7841be15d8022c2257f672b84ffa6b5696a5f8a
Max <max_pirate@mail.ru>
2022-08-08 15:15:43 +0300

    Add API

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
     <h1>Events</h1>
+    <event-card
+      v-for="(event, index) in events"
+      :key="event.id"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
-export default{
-  head(){
+import EventCard from '@/components/EventCard.vue';
+export default {
+  components:{
+    EventCard
+  },
+  asyncData({ $axios }) {
+    return $axios
+      .get(
+        'https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router/events'
+      )
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+  },
+  head() {
     return {
       title: 'Event Listing',
     }
-  }
+  },
 }
 </script>
